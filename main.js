5ef6e11ccbce561fb56d6968f1fb3aee22945105
Kewbish <kewbish@gmail.com>
2020-12-14 20:05:45 -0800

    Wrap into function, add date

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const FEEDS = [
 ];
 const CORS = url => `https://cors-anywhere.herokuapp.com/${encodeURIComponent(url)}`
 
-// test to see if template works nicely
-const main = document.querySelector(".m-matter");
+const main = document.querySelector(".grid");
 const article = document.querySelector("#m-item");
-var clone = article.content.cloneNode(true);
-clone.querySelector("a").href = "http://kewbi.sh";
-clone.querySelector("h2").innerText = "Main page title";
-clone.querySelector("p").innerText = "This is a main page.";
-main.appendChild(clone);
+
+function addItem(ln, title, desc, date) {
+    var clone = article.content.cloneNode(true);
+    clone.querySelector("a").href = ln;
+    clone.querySelector("h2").innerText = title;
+    clone.querySelector("p").innerText = `${desc} - published ${date.toDateString()}`;
+    main.appendChild(clone);
+}
+
+addItem("https://kewbi.sh", "A main page", "A description for said main page", new Date("14 Dec 2020"));
+addItem("https://kewbi.sh/blog/posts/201206/", "Morse Code Context", "I wrote a thing about dots and dashes, enjoy!", new Date("06 Dec 2020"));
 
