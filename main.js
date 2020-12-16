1636edb70c596346b1987e87954de7a446239d04
Kewbish <kewbish@gmail.com>
2020-12-15 17:07:26 -0800

    Add feed parser

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 const FEEDS = [
     'https://kewbi.sh/blog/index.xml',
 ];
-const CORS = url => `https://cors-anywhere.herokuapp.com/${encodeURIComponent(url)}`
 
 const main = document.querySelector(".grid");
 const article = document.querySelector("#m-item");
+var feed_dates = {}
 
-function addItem(ln, title, desc, date) {
+function addItem(ln, title, desc) {
     var clone = article.content.cloneNode(true);
     clone.querySelector("a").href = ln;
     clone.querySelector("h2").innerText = title;
-    clone.querySelector("p").innerText = `${desc} - published ${date.toDateString()}`;
+    clone.querySelector("p").innerText = `${desc}`;
     main.appendChild(clone);
 }
 
-addItem("https://kewbi.sh", "A main page", "A description for said main page", new Date("14 Dec 2020"));
-addItem("https://kewbi.sh/blog/posts/201206/", "Morse Code Context", "I wrote a thing about dots and dashes, enjoy!", new Date("06 Dec 2020"));
+function parseFeed(feed) {
+    fetch(feed, { method: "GET" })
+        .then(text => text.text())
+        .then(texml => {
+            const rss = new DOMParser().parseFromString(texml, 'text/xml');
+            const items = rss.querySelectorAll("item");
+            console.log(items);
+        });
+        
+}
+
+addItem("https://kewbi.sh", "A main page", "A description for said main page");
+
+parseFeed('https://kewbi.sh/blog/index.xml');
 
