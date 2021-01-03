428d51ecadcfeec47b24f53aafbac72094dc04be
Kewbish <kewbish@gmail.com>
2021-01-02 20:02:21 -0800

    Add network-first SW

--- sw.js
+++ sw.js
@@ -15,10 +15,31 @@ self.addEventListener('install', ins => {
   );
 });
 
+const fromNetwork = (req, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(req).then(res => {
+      clearTimeout(timeoutId);
+      fulfill(res);
+      update(req);
+    }, reject);
+  });
+
+const fromCache = req =>
+  caches.open('matter')
+  .then(cache =>
+    cache.match(req)
+    .then(matching => matching || cache.match('index.html'))
+  );
+
+const update = req =>
+  caches.open('matter')
+  .then(cache =>
+    fetch(req).then(res => cache.put(req, res))
+  );
+
 self.addEventListener('fetch', ins => {
   ins.respondWith(
-    caches.match(ins.request).then(res => {
-      return res || fetch(ins.request);
-    }),
-  );
+    fromNetwork(ins.request, 5000).catch(() => fromCache(ins.request)));
+  ins.waitUntil(update(ins.request));
 });
